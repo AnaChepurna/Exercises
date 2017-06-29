5702a114e6ddf234dd436824075111e680de36d8
AnaChepurna <ch.nastasie@gmail.com>
2017-06-29 23:16:16 +0300

    new javarush

---
+++ task2409/Item.java
@@ -0,0 +1,13 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by Ana on 29.06.2017.
+ */
+interface Item {
+
+    public int getId();
+
+    public double getPrice();
+
+    public String getTM();
+}
---
+++ task2409/Jeans.java
@@ -0,0 +1,11 @@
+package com.javarush.task.task24.task2409;
+
+/**
+ * Created by Ana on 29.06.2017.
+ */
+interface Jeans extends Item {
+
+    public int getLength();
+
+    public int getSize();
+}
