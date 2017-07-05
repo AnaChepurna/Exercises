19d9105cf1e5aa319b6d35c790d1414241c39f55
AnaChepurna <ch.nastasie@gmail.com>
2017-07-05 14:54:28 +0300

    javarush big

---
+++ FactoryProducer+ObjectFactory/AbstractFactory.java
@@ -0,0 +1,8 @@
+package com.javarush.task.task37.task3702;
+
+/**
+ * Created by Ana on 03.07.2017.
+ */
+public interface AbstractFactory {
+    public Human getPerson(int age);
+}
---
+++ FactoryProducer+ObjectFactory/Human.java
@@ -0,0 +1,7 @@
+package com.javarush.task.task37.task3702;
+
+/**
+ * Created by Ana on 03.07.2017.
+ */
+public interface Human {
+}
